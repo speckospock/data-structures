227e337324b8e30f7deeff5505a7b92fa412f569
speckospock <ben.hq.stephens@gmail.com>
2017-08-23 16:06:21 -0700

    Implemented pseudocode for functional-shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,20 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //create a new object to be our stack
+  //create a storage object that is a property of stack
+  //create a number to represent size of the stack storage object
+  //extend stack with stackMethods
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  //create a push function with key 'push' with parameter value
+    //add value to stack storage
+    //increment stack size
+  //create a pop function with no parameters with key 'pop'
+    //decrement stack size
+    //return value at stack storage[size]
+  //create a size function with key 'size'
+    //return size (created at line 7)
+};
