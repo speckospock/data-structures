4ce01c2829ce1feacab7dca7010b3ac1b6b97165
speckospock <ben.hq.stephens@gmail.com>
2017-08-23 16:12:18 -0700

    Implemented functional-shared stack constructor, stackMethods size and push, passed tests 1 and 2

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -3,18 +3,31 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
 
   //create a new object to be our stack
+  var newStack = {};
   //create a storage object that is a property of stack
+  newStack.storage = {};
   //create a number to represent size of the stack storage object
+  newStack.count = 0;
   //extend stack with stackMethods
+  _.extend(newStack, stackMethods);
+  //return the stack
+  return newStack;
 };
 
 var stackMethods = {
   //create a push function with key 'push' with parameter value
+  'push' : function(value){
     //add value to stack storage
+    this.storage[this.count] = value;
     //increment stack size
+    this.count++;
+  },
   //create a pop function with no parameters with key 'pop'
     //decrement stack size
     //return value at stack storage[size]
   //create a size function with key 'size'
+  'size' : function(){
     //return size (created at line 7)
+    return this.count;
+  }
 };
