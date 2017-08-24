1281e4494672f35c769d789d2e2167312397fbe5
speckospock <ben.hq.stephens@gmail.com>
2017-08-23 20:08:06 -0700

    Complete pseudoclassical queue constructor and prototype properties highest, lowest, storage, size. Passed first test

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,21 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //
 };
 
-
+//Add things to the prototype here
+//Add a storage property to queue prototype
+Queue.prototype.storage = {};
+//Add a highest property to queue prototype
+Queue.prototype.highest = 0;
+//Add a lowest property to queue prototype
+Queue.prototype.lowest = 0;
+//Add a size method to queue prototype
+Queue.prototype.size = function () {
+  //return the highest - lowest
+  return this.highest - this.lowest;
+};
+//Add an enqueue method to queue prototype
+//Add an dequeue method to queue prototype
