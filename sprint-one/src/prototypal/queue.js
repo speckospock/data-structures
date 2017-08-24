9e2e76d154aa2a868549e47c08761f3089e947d5
speckospock <ben.hq.stephens@gmail.com>
2017-08-23 19:51:39 -0700

    Implement prototypal queue constructor function, size method

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // create an object with Object.create to represent queue
+  var newQ = Object.create(queueMethods);
+  // return queue
+  return newQ;
 };
 
-var queueMethods = {};
-
-
+var queueMethods = {
+  // Create a storage obj (empty)
+  'storage': {},
+  // Create a highest property, 0
+  'highest': 0,
+  // Create a lowest prop, 0
+  'lowest': 0,
+  // Create a size function
+  'size': function () {
+    // Return highest-lowest
+    return this.highest - this.lowest;
+  },
+  // Create enqueue function with param value
+    // Decrement lowest
+    // Set storage[lowest] to value
+  // Create dequeue function, no parameters
+    // iff highest > lowest, do:
+      // decrement highest
+      // return storage[highest]
+};
