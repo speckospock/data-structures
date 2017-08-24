33afbd3649468b9d776ba08bfbb0d49bfdea3163
speckospock <ben.hq.stephens@gmail.com>
2017-08-23 19:40:41 -0700

    Implemented prototypal stack constructor function and size stackMethod, passed first test

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  //we're using Object.create(...)
+
+  //create new obj using Object.create(stackMethods)
+  var newStack = Object.create(stackMethods);
+  //return object
+  return newStack;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  //create a count property, set it to zero
+  'count': 0,
+  //create a storage property, set it to an empty object
+  'storage': {},
+  //create a size property, set it to a function with no parameters
+  'size': function() {
+    //return the stack storage object's size
+    return this.count;
+  },
+  //create a push property, set it to a function with parameter value
+    //add value to storage at key size
+    //increment size
+  //create a pop property, set it to a function with no parameters
+    //iff stack size > 0
+      //decrement count
+      //return the value at stack storage[count]
+};
