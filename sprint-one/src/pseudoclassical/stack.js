48c8bc4d49a41995de868a29bf2b1658b7c77f8d
speckospock <ben.hq.stephens@gmail.com>
2017-08-23 20:21:52 -0700

    Implement pseudoclassical stack constructor, prototype method size

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // Add a count property to the instance stack
+  this.count = 0;
+  // Add a storage property to the instance stack
+  this.storage = {};
 };
 
-
+// Add a size method to stack proto
+Stack.prototype.size = function () {
+  // Return this stack's count
+  return this.count;
+};
+// Add a push method to stack proto with param value
+  // Increment this stack's count
+  // Set this stack's storage[count] to value
+// Add a pop method to stack proto
+  // iff this stack's count > 0:
+    // decrement count
+    // return value at this stack's storage[count]
