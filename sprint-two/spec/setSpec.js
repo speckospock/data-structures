5b8a9643df91e382a71d073d505e28ede3cecc1f
speckospock <ben.hq.stephens@gmail.com>
2017-08-24 20:44:18 -0700

    Add tests for duplication and contains

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -18,10 +18,23 @@ describe('set', function() {
     expect(set.contains('Susan Sarandon')).to.equal(true);
   });
 
+  it('should return whether a value is contained in a set', function() {
+    set.add('Peter Dinklage');
+    expect(set.contains('Peter Dinklage')).to.equal(true);
+    set.remove('Peter Dinklage');
+    expect(set.contains('Peter Dinklage')).to.equal(false);
+  });
+
   it('should remove values from a set', function() {
     set.add('Mel Gibson');
     set.remove('Mel Gibson');
     expect(set.contains('Mel Gibson')).to.equal(false);
   });
 
+  it('should not allow duplicate values to be added to a set', function() {
+    set.add('Peter Dinklage');
+    set.add('Peter Dinklage');
+    set.remove('Peter Dinklage');
+    expect(set.contains('Peter Dinklage')).to.equal(false);
+  })
 });
