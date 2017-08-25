ad4e6f187dbfbe081c4a3740e1a6a7fcabe0b5f8
speckospock <ben.hq.stephens@gmail.com>
2017-08-25 14:42:36 -0700

    Implemented prototype for BinarySearchTree, and modified test to use keyword 'new' for pseudoclassical pattern

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -2,7 +2,7 @@ describe('binarySearchTree', function() {
   var binarySearchTree;
 
   beforeEach(function() {
-    binarySearchTree = BinarySearchTree(5);
+    binarySearchTree = new BinarySearchTree(5); //to use pseduoclassical
   });
 
   it('should have methods named "insert", "contains", and "depthFirstLog', function() {
@@ -31,6 +31,7 @@ describe('binarySearchTree', function() {
   it('should execute a callback on every value in a tree using "depthFirstLog"', function() {
     var array = [];
     var func = function(value) { array.push(value); };
+    // binarySearchTree.insert(5) ??
     binarySearchTree.insert(2);
     binarySearchTree.insert(3);
     binarySearchTree.depthFirstLog(func);
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,11 @@
 var BinarySearchTree = function(value) {
 };
 
+BinarySearchTree.prototype.insert = function() {};
+
+BinarySearchTree.prototype.contains = function() {};
+
+BinarySearchTree.prototype.depthFirstLog = function () {};
 
 /*
  * Complexity: What is the time complexity of the above functions?
