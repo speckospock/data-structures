7319d8dc065154cbcc73d8cf5074ea98834affdb
speckospock <ben.hq.stephens@gmail.com>
2017-08-24 20:50:55 -0700

    Provide set add and contain methods and _storage property

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,23 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // assign value at set storage at key item to true
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  // return set storage at key item or false
+  return this._storage[item] || false;
 };
 
 setPrototype.remove = function(item) {
+  // assign value at set storage at key item to false
 };
 
 /*
