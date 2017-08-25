b04762f57aafa8163caa96d51a6cb9d1e02a96b0
speckospock <ben.hq.stephens@gmail.com>
2017-08-25 15:15:23 -0700

    Provide BinarySearchTree insert method, pass test

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,11 +1,44 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
 };
 
-BinarySearchTree.prototype.insert = function() {};
+BinarySearchTree.prototype.left; //at some point, this will be a binarySearchTree
+
+BinarySearchTree.prototype.right;
+
+BinarySearchTree.prototype.insert = function(value) {
+  //depthFirstLog will NOT be used. For now >.>
+  //compare tree to value
+  if (this.value < value) {
+  //iff tree < value:
+    //check if there's a right child
+    if (this.right) {
+      //if so: call tree.right.insert(value)
+      this.right.insert(value);
+    } else {
+      //if not: tree.right = BST(value)
+      this.right = new BinarySearchTree(value);
+    }
+  } else if (this.value > value) {
+  //else iff tree > value:
+    //check if there's a left child
+    if (this.left) {
+      //if so: call tree.left.insert(value)
+      this.left.insert(value);
+    } else {
+      //if not: tree.left = BST(value)
+      this.left = new BinarySearchTree(value);
+    }  
+  }
+};
 
 BinarySearchTree.prototype.contains = function() {};
 
-BinarySearchTree.prototype.depthFirstLog = function () {};
+BinarySearchTree.prototype.depthFirstLog = function (callback) {
+  //apply callback to top node (this?)
+  //for the left child, apply depthFirstLog(callback)
+  //for the right child, apply depthFirstLog(callback)
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
