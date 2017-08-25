e15c39e1ecda99462857435ab1324a824af6295a
speckospock <ben.hq.stephens@gmail.com>
2017-08-25 10:44:05 -0700

    Refactor pseudocode for hashTable to remove objects in favor of arrays

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,38 +3,40 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-  // set hash function to = memoized getIndex... by
-  // _.memoize(getIndex...)
-  this._newHash = _.memoize(getIndexBelowMaxForKey);
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = this._newHash(k, this._limit);
-  var newObj = {};
-  newObj[k] = v;
+  var index = this.getIndexBelowMaxForKey(k, this._limit);
+  //
   // if this._storage[index] is undefined:
   if (this._storage.get(index) === undefined) {
-    // set it to {k:v}
-    this._storage.set(index, newObj);
-  } else {
-  // else
-    // _.extend this._storage[index] with {k:v}
-    _.extend(this._storage.get(index), newObj);
+    this._storage.set(index, []);
   }
+  // iterate over bucket
+
+    // if bucket[i][0] is key
+      // set bucket[i][1] to value
+    // else push into bucket [k,v]
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = this._newHash(k, this._limit);
-  // return this._storage[index][k]
-  return this._storage.get(index)[k];
+  var index = this.getIndexBelowMaxForKey(k, this._limit);
+  // iterate over bucket
+    // if bucket[i][0] === k
+      // return bucket[i][1]
+
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = this._newHash(k, this._limit);
+  var index = this.getIndexBelowMaxForKey(k, this._limit);
   // if this._storage[index][k] not undefined
   if (this._storage.get(index) !== undefined) {
-    // delete this._storage[index][k]
-    delete this._storage.get(index)[k];
+    var toDelete;
+    // iterate over bucket
+      // if bucket[i][0] = k
+        // toDelete = i
+    // if toDelete is defined
+      // delete bucket[i]
   }
 };
 
