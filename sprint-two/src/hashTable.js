7cfad3323a9197f4a4e991d32b64f1d9a3f8e617
speckospock <ben.hq.stephens@gmail.com>
2017-08-25 11:08:01 -0700

    Provide array-based implementations of hashTable contains and add

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,34 +1,44 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = this.getIndexBelowMaxForKey(k, this._limit);
-  //
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  debugger;
   // if this._storage[index] is undefined:
   if (this._storage.get(index) === undefined) {
     this._storage.set(index, []);
+    this._storage.get(index).push([k,v]);
+  } else {
+    // iterate over bucket
+    for (let i = 0; i < this._storage.get(index).length; i++) {
+      // if bucket[i][0] is key
+      if (this._storage.get(index)[i][0] === k) {
+        // set bucket[i][1] to value
+        this._storage.get(index)[i][1] = v;
+      } else {
+      // else push into bucket [k,v]
+        this._storage.get(index).push([k,v]);
+      }
+    }
   }
-  // iterate over bucket
-
-    // if bucket[i][0] is key
-      // set bucket[i][1] to value
-    // else push into bucket [k,v]
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = this.getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this._limit);
   // iterate over bucket
+  for (let i = 0; i < this._storage.get(index).length; i++) {
     // if bucket[i][0] === k
+    if (this._storage.get(index)[i][0] === k) {
       // return bucket[i][1]
-
+      return this._storage.get(index)[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = this.getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this._limit);
   // if this._storage[index][k] not undefined
   if (this._storage.get(index) !== undefined) {
     var toDelete;
